09ef24fe19556b2e581c4d9a5dc6ad04a12f3f2c
Christopher Cruzen <christopher.a.cruzen@gmail.com>
2024-09-21 15:15:19 -0500

    Update Icons & Notes

--- Source/Build Scripts/RebuildHaloMenuMod.bat
+++ Source/Build Scripts/RebuildHaloMenuMod.bat
@@ -12,12 +12,19 @@ echo on
 
 @echo. && echo /////////////////////// && echo /// Cooking Project /// && echo /////////////////////// && echo.
 
-:: Cook Unreal Project
+:: Cook Unreal Assets
 @call "%engine_dir%\Build\BatchFiles\RunUAT.bat" BuildCookRun -nocompile -nocompileeditor -installed -nop4 -project="%repo_dir%\Source\Unreal Project\MCC\MCC.uproject" -cook -skipstage -ue4exe=UE4Editor-Cmd.exe -targetplatform=Win64 -utf8output
+
+@echo. && echo ///////////////////// && echo /// Cleaning Cook /// && echo ///////////////////// && echo.
+
+:: Remove Extra Cook Files
+rmdir "%repo_dir%\Source\Unreal Project\MCC\Saved\Cooked\WindowsNoEditor\MCC\Metadata" /s /q
+del "%repo_dir%\Source\Unreal Project\MCC\Saved\Cooked\WindowsNoEditor\MCC\AssetRegistry.bin"
+echo Deleting generated cook files.
 
 @echo. && echo /////////////////////////// && echo /// Generating Pak File /// && echo /////////////////////////// && echo.
 
-:: Build Pak in Final Mod Directory
+:: Compress Cooked Assets into Pak in Final Mod Directory
 @call "%engine_dir%\Binaries\Win64\UnrealPak.exe" "%game_dir%\MCC\Content\Paks\~mods\MCC-WindowsNoEditor_P.pak" -Create="%repo_dir%\Source\Build Scripts\ContentToRepack.txt"
 
 @echo. && echo ////////////////////// && echo /// Build Complete /// && echo ////////////////////// && echo.
